9652d3cfecfe31b49f23e6a0e176274a3ca02ece
doktor alban <doktoralban@outlook.com>
2020-06-26 18:10:50 +0300

    Create Test.cs

---
+++ AppGetDataFromWebApi/AppGetDataFromWebApi/Models/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGetDataFromWebApi.Models
+{
+    public  class Test
+    {
+
+    public int XProductID { get; set; }
+    public string  XProductName { get; set; }
+    public decimal XUnitPrice { get; set; }
+
+
+
+
+    }
+}
